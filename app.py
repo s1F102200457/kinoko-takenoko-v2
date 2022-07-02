fc53a5fad13fa218fef635a38df56c5511dcb90a
s1F102200457 <107388448+s1F102200457@users.noreply.github.com>
2022-07-02 14:28:24 +0900

    aaa

--- app.py
+++ app.py
@@ -3,16 +3,30 @@ app = Flask(__name__)
 
 kinoko_count=3
 takenoko_count=5
-massages=["kinoko is wonderfull!","Takenoko is awesome!"]
+messages=["kinoko is wonderfull!","Takenoko is awesome!"]
 
 @app.route('/')
 def top():
-    kinoko_percent=kinoko_count/(kinoko_count+takenoko_count)*100
-    takenoko_percent=takenoko_count/(kinoko_count+takenoko_count)*100
     return render_template('index.html', **vars())
 
 @app.route('/vote', methods=['POST'])
 def answer():
+    print(request.form)
+    global kinoko_count,takenoko_count,messages
+    if request.form.get("item")=="kinoko":
+        kinoko_count+=1
+    elif request.form.get("item")=="takenoko":
+        takenoko_count+=1
+
+        messages.append(request.form.get("message"))
+        if len(massages)>3:
+            messages=messages[-3:]
+    kinoko_percent=kinoko_count/(kinoko_count+takenoko_count)*100
+    takenoko_percent=takenoko_count/(kinoko_count+takenoko_count)*100
+    message_html=""
+    for i in range(len(messages)):
+        message=message[i]
+        message_html+="<div class='alert{1}' role='alert'>{0}</div>\n' .format(message, 'alert-warning ms-5' if i % 2==0 else 'alert-success me-5"
     return render_template('vote.html', **vars())
 
 if __name__ == '__main__':
